861f6fb57760e8a99da7d975beb93350c2edce0f
John Abed <john.abed@hotmail.com>
2019-04-09 22:53:22 -0400

    Created Interactable type for world objects that player will be able to interact with

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 1f; // how close to interact
+    public Transform interactionTransform; //where the player can interact with the Interactable (for chests etc)
+
+    bool isFocus = false;
+    Transform player;
+
+    bool hasInteracted = false;
+
+    public virtual void Interact ()
+    {
+        // This method is meant to be overwritten
+        Debug.Log("Interacting with " + transform.name);
+    }
+
+    void Update()
+    {
+        if (isFocus && !hasInteracted)
+        {
+            float distance = Vector2.Distance(player.position, interactionTransform.position);
+            if (distance <= radius)
+            {
+                Interact();
+                hasInteracted = true;
+            }
+        }
+    }
+
+    public void OnFocused (Transform playerTransform)
+    {
+        GetComponent<SpriteRenderer>().color = Color.red;
+        isFocus = true;
+        player = playerTransform;
+        hasInteracted = false;
+    }
+
+    public void OnDefocused()
+    {
+        GetComponent<SpriteRenderer>().color = Color.white;
+        isFocus = false;
+        player = null;
+        hasInteracted = false;
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(interactionTransform.position, radius);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,11 +12,16 @@ public class PlayerController : MonoBehaviour
     private bool playerMoving;
     private Vector2 lastMove;
 
+    public Camera cam;
+    public LayerMask movementMask;
+    public Interactable focus;
+
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody2D>();
+        cam = Camera.main;
     }
 
     // Update is called once per frame
@@ -26,6 +31,8 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
         {
+            RemoveFocus();
+
             //transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
             myRigidbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, myRigidbody.velocity.y);
             playerMoving = true;
@@ -33,6 +40,8 @@ public class PlayerController : MonoBehaviour
         }
         if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
         {
+            RemoveFocus();
+
             //transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
             myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed);
             playerMoving = true;
@@ -53,5 +62,67 @@ public class PlayerController : MonoBehaviour
         anim.SetBool("PlayerMoving", playerMoving);
         anim.SetFloat("LastMoveX", lastMove.x);
         anim.SetFloat("LastMoveY", lastMove.y);
+
+        //Check for rightmouse click on Interactables
+        if (Input.GetMouseButtonDown(1))
+        {   
+            //Create a ray
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            //Check if ray hits
+            if(Physics.Raycast(ray, out hit, 100))
+            {
+                print("rightclick hit"); //Todo: Remove
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                //Did we hit an interactable
+                if(interactable != null)
+                {
+                    //Set our focus to the object
+                    SetFocus(interactable);
+                }
+            }
+        }
+    }
+
+    //Focus on a specific interactable object
+    void SetFocus(Interactable newFocus)
+    {
+        print("SetFocus"); //Todo: Remove
+
+        //if new focus
+        if (newFocus != focus)
+        {
+            if(focus != null)
+            {
+                focus.OnDefocused();
+            }
+
+            focus = newFocus;
+        }
+
+        newFocus.OnFocused(transform);
+        FaceFocus();
+    }
+
+    void RemoveFocus()
+    {
+        print("RemoveFocus"); //Todo: Remove
+
+        if (focus != null)
+        {
+            focus.OnDefocused();
+        }
+        focus = null;
+    }
+
+    void FaceFocus()
+    {
+        //Todo: DEBUG CLOTHING NOT ROTATING BECAUSE THEY DONT SHARE lastMove VARIABLE!!!
+
+        //get dist from object
+        Vector2 direction = (focus.transform.position - transform.position).normalized;
+        lastMove.x = direction.x;
+        lastMove.y = direction.y; 
     }
 }
