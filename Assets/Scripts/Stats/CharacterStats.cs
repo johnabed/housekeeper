97d3a72b6722541f7ed6b9259eefd467e69adf99
John Abed <john.abed@hotmail.com>
2019-04-15 14:15:20 -0400

    Working on Armor Stats and player damage

---
+++ Assets/Scripts/Stats/CharacterStats.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+    public int maxHealth = 100; //not Stat as we don't plan to add modifiers
+    public int currentHealth { get; private set; }
+   
+    public Stat damage;
+    public Stat armor;
+
+    void Awake ()
+    {
+        currentHealth = maxHealth;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            TakeDamage(10);
+        }
+    }
+
+    public void TakeDamage (int damage)
+    {
+        damage -= armor.GetValue(); //mitigate damage with armor protection
+        damage = Mathf.Clamp(damage, 0, int.MaxValue); //ensure value is positive
+
+        currentHealth -= damage;
+        Debug.Log(transform.name + " takes " + damage + " damage.");
+
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public virtual void Die()
+    {
+        //Die in some way
+        //This is method is meant to be overridden
+        Debug.Log(transform.name + " died.");
+    }
+}
